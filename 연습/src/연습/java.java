66732a7dda301e712c56e7c56ee0c6da6c61982d
SIST <SIST@DESKTOP-6GQQA6U>
2020-07-02 17:26:05 +0900

    0

---
+++ 연습/src/연습/java.java
@@ -0,0 +1,16 @@
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		
+		a=3;
+		b=a+5;
+		
+		System.out.printf("a의값: %d \n",a);
+		System.out.printf("b의값: %d \n",b);
+	}
+
+}
---
+++ 연습/src/연습/java2.java
@@ -0,0 +1,10 @@
+package ¿¬½À;
+
+public class java2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
