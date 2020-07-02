6e025405818c1e8db7f123df6f289fb104f2eeba
SIST <SIST@DESKTOP-6GQQA6U>
2020-07-02 17:26:36 +0900

    0

---
+++ 연습2/src/연습2/java.java
@@ -0,0 +1,135 @@
+package 연습2;
+
+import java.util.*;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int a;
+		int b;
+		
+		a=3;
+		b=a+5;
+		
+		System.out.printf("a의값: %d \n",a);
+		System.out.printf("b의값: %d \n",b);
+
+		
+		int a,b;
+		a=6;
+		b=2;
+		
+		System.out.printf("덧셈 연산 결과: %d \n",a+b);
+		System.out.printf("뺄셈 연산 결과: %d \n",a-b);
+		System.out.printf("곱셈 연산 결과: %d \n",a*b);
+		System.out.printf("나누기 연산 결과: %d \n",a/b);
+		System.out.printf("나머지 연산 결과: %d \n",a%b);
+		
+		
+		int num1,num2;
+		num1=10/3;
+		//몫 출력
+		num2=10%3;
+		//나머지 출력
+		
+		System.out.printf("몫:%d \n",num1);
+		System.out.printf("나머지:%d \n",num2);
+		
+		
+		int num1=1, num2=2, num3=3, num4=4, num5=5;
+		num1=num1+num2;
+		num2=num2-2;
+		num3=num3*2;
+		num4=num4/2;
+		num5=num5%2;
+		System.out.printf("%d, %d, %d, %d, %d \n",num1,num2,num3,num4,num5);
+		
+		
+		int num1=10;
+		System.out.printf("%d \n",num1);
+		num1++;
+		System.out.printf("%d \n",num1);
+		++num1;
+		System.out.printf("%d \n",num1);
+		--num1;
+		System.out.printf("%d \n",num1);
+		num1--;
+		System.out.printf("%d \n",num1);
+		
+		
+		
+		int num1=10, num2=10;
+		int a,b;
+		a = ++num1;
+		b = num2++;
+		
+		System.out.printf("%d ,%d \n",b,num2);
+		
+		
+		int num1=10,num2=10;
+		int a,b;
+		num1 = num1+1;
+		a= num1;
+		System.out.printf("%d %d \n",a,num1);
+		b= num2;
+		num2= num2+1;
+		System.out.printf("%d %d \n",b,num2);
+		
+		
+		int num1=10,num2=10;
+		System.out.printf("%d \n",++num1);
+		System.out.printf("%d \n",num1);
+		System.out.printf("%d \n",num2++);
+		System.out.printf("%d \n",num2);
+	
+		
+
+		int num1=2;
+		int num2=7;
+		int num3;
+		int num4;
+
+		num1++;
+		num3 = --num1;
+		--num2;
+		num4=num2++;
+		
+		
+		System.out.printf("%d \n",num3);
+		System.out.printf("%d \n",num4);
+		
+		
+		int i=0;
+		int re=0;
+		i=3;
+		re=++i;
+		
+		System.out.println("re:"+re+",i:"+i);
+		i=3;
+		re=i++;
+		System.out.println("re:"+re+",i:"+i);
+		
+		*/
+		int a,b;
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.print("두개의 정수입력:");
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		int a,b;
+		
+		
+		System.out.println("뺄셈결과:",a-b);
+		System.out.println("곱셈결과:",a*b);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
